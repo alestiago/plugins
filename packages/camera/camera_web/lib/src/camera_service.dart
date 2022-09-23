573bfb0a8d6eb44a507731cee1bb9203d6cd9d24
alestiago <dev@alestiago.com>
2022-09-23 12:02:19 +0100

    fix: ignore constraints

--- packages/camera/camera_web/lib/src/camera_service.dart
+++ packages/camera/camera_web/lib/src/camera_service.dart
@@ -47,7 +47,7 @@ class CameraService {
 
     try {
       final Map<String, dynamic> constraints = options.toJson();
-      return await mediaDevices.getUserMedia(constraints);
+      return await mediaDevices.getUserMedia();
     } on html.DomException catch (e) {
       switch (e.name) {
         case 'NotFoundError':
